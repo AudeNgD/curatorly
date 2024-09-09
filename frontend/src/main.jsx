3a013b2dc823d28ad3c79bcc41b4fba232ad8fc7
AudeNgD <aude.nguyenduc@gmail.com>
2024-09-09 16:19:02 +0100

    setup scene

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import * as THREE from "three";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
