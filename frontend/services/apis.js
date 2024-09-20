21a728187905c67448529d14e219a2bd7c5a0bce
AudeNgD <aude.nguyenduc@gmail.com>
2024-09-20 15:40:35 +0100

    add pagination to handle large responses

--- frontend/services/apis.js
+++ frontend/services/apis.js
@@ -1,5 +1,21 @@
 import axios from "axios";
 
+const chicagoDataAPI = axios.create({
+  baseURL: `https://api.artic.edu/api/v1/artworks`,
+});
+
+const chicagoImageAPI = axios.create({
+  baseURL: `https://www.artic.edu/iiif/2/`,
+});
+
+const metSearchAPI = axios.create({
+  baseURL: `https://collectionapi.metmuseum.org/public/collection/v1/search`,
+});
+
+const metObjectsAPI = axios.create({
+  baseURL: `https://collectionapi.metmuseum.org/public/collection/v1/objects`,
+});
+
 const europeanaAPI = axios.create({
   baseURL: `https://api.europeana.eu/record/v2/search.json`,
   headers: {
@@ -20,18 +36,30 @@ export const fetchArtworks = (params) => {
   const artistName = params[0].get("aname");
   const europeanaChecked = params[0].get("echeck");
   const rijksmuseumChecked = params[0].get("rcheck");
+  const metChecked = params[0].get("mcheck");
+  const chicagoChecked = params[0].get("ccheck");
 
   let europeanaPromise = null;
   let rijskPromise = null;
+  let metPromise = null;
+  let chicagoPromise = null;
 
   //if artist name is empty, return all top artworks from both museums
   if (artistName === "") {
     const rijksQuery = `?key=${
       import.meta.env.VITE_APP_RIJKS_API_KEY
     }&toppieces=true`;
+
     const europeanaQuery = `?query=who:*`;
     rijskPromise = rijksAPI.get(rijksQuery);
     europeanaPromise = europeanaAPI.get(europeanaQuery);
+
+    // const metQuery = ``;
+    // metPromise = metSearchAPI.get(metQuery);
+    metPromise = metObjectsAPI.get();
+
+    const chicagoQuery = `?limit=100`;
+    chicagoPromise = chicagoDataAPI.get(chicagoQuery);
   }
 
   if (artistName !== "") {
@@ -46,12 +74,11 @@ export const fetchArtworks = (params) => {
   //send only the promises for the museums that have been checked
   //as a param it is a string
   let promises = [];
-  if (europeanaChecked === "true") {
-    promises.push(europeanaPromise);
-  }
-  if (rijksmuseumChecked === "true") {
-    promises.push(rijskPromise);
-  }
+
+  europeanaChecked === "true" ? promises.push(europeanaPromise) : null;
+  rijksmuseumChecked === "true" ? promises.push(rijskPromise) : null;
+  metChecked === "true" ? promises.push(metPromise) : null;
+  chicagoChecked === "true" ? promises.push(chicagoPromise) : null;
 
   if (promises.length === 0) {
     return Promise.resolve(null);
@@ -60,18 +87,22 @@ export const fetchArtworks = (params) => {
   return Promise.all(promises).then((results) => {
     let data = {};
 
-    if (results.length === 1) {
-      if (results[0].data.items) {
-        return (data = { items: results[0].data.items, artObjects: [] });
-      } else {
-        return (data = { items: [], artObjects: results[0].data.artObjects });
+    if (results && results.length >= 1) {
+      for (let i = 0; i < results.length; i++) {
+        //check from which api data is coming
+        if (results[i].data.artObjects) {
+          data.rijksData = results[i].data.artObjects;
+        } else if (results[i].data.items) {
+          data.europeanaData = results[i].data.items;
+        } else if (results[i].data.objectIDs) {
+          data.metData = results[i].data.objectIDs;
+        } else if (results[i].data.data) {
+          data.chicagoData = results[i].data.data;
+        }
       }
-    } else if (results.length === 2) {
-      return (data = {
-        items: results[0].data.items,
-        artObjects: results[1].data.artObjects,
-      });
     }
+
+    console.log("here in apis", data);
     return data;
   });
 };
--- frontend/src/components/ResultsList.jsx
+++ frontend/src/components/ResultsList.jsx
@@ -2,10 +2,64 @@ import React, { useState, useEffect } from "react";
 import ArtworkCard from "./ArtworkCard";
 
 export default function ResultsList({ artworks }) {
-  const results = artworks;
+  const allResults = artworks;
+  const [artworksPerPage, setArtworksPerPage] = useState(10);
+  const totalNbrofPages = Math.ceil(allResults.length / artworksPerPage);
+  const [currentPageIndex, setCurrentPageIndex] = useState(0);
+  // const [results, setResults] = useState(
+  //   allResults.slice(
+  //     currentPageIndex * artworksPerPage,
+  //     currentPageIndex * artworksPerPage + artworksPerPage
+  //   )
+  // );
+
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    setResults(
+      allResults.slice(
+        currentPageIndex * artworksPerPage,
+        currentPageIndex * artworksPerPage + artworksPerPage
+      )
+    );
+  }, [artworks]);
+
+  useEffect(() => {
+    console.log("in results list", results);
+    const artworksToDisplay = allResults.slice(
+      currentPageIndex * artworksPerPage,
+      currentPageIndex * artworksPerPage + artworksPerPage
+    );
+    console.log(artworksToDisplay);
+    setResults(artworksToDisplay);
+  }, [currentPageIndex]);
+
+  function handleClickNext() {
+    setCurrentPageIndex(currentPageIndex + 1);
+  }
+
+  function handleClickPrevious() {
+    setCurrentPageIndex(currentPageIndex - 1);
+  }
+
   return (
     <div id="results-container">
-      {artworks ? <ArtworkCard artworks={results} /> : <p>No results found</p>}
+      {results ? (
+        <ArtworkCard artworks={results} count={allResults.length} />
+      ) : (
+        <p>No results found</p>
+      )}
+      <div id="results-pagination">
+        {currentPageIndex > 0 ? (
+          <button onClick={handleClickPrevious}>Previous</button>
+        ) : null}
+        <p>
+          {currentPageIndex + 1}/{totalNbrofPages}
+        </p>
+        {currentPageIndex < totalNbrofPages - 1 ? (
+          <button onClick={handleClickNext}>Next</button>
+        ) : null}
+      </div>
     </div>
   );
 }
