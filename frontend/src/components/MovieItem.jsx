1908391d9b6d383c0a050cfaee0c74e679098bfc
AudeNgD <aude.nguyenduc@gmail.com>
2024-09-28 13:07:30 +0100

    add pause movie animation

--- frontend/src/components/MovieItem.jsx
+++ frontend/src/components/MovieItem.jsx
@@ -1,27 +1,36 @@
 import React, { useState } from "react";
 
-function MovieItem({ imageUrl, title, id }) {
+function MovieItem({ artworkForFrame, paused, setPaused }) {
   const [displayInfo, setDisplayInfo] = useState(false);
+
+  console.log("artworkForFrame", artworkForFrame);
 
   const mouseEnter = (e) => {
     setDisplayInfo(true);
+    setPaused(true);
   };
 
   const mouseLeave = (e) => {
     setDisplayInfo(false);
+    setPaused(false);
   };
 
   return (
     <div className="movie-frame">
       <img
-        src={imageUrl}
-        alt={title}
+        src={artworkForFrame.imageUrl}
+        alt={artworkForFrame.title}
+        className=""
         onMouseEnter={(e) => mouseEnter(e)}
         onMouseLeave={(e) => mouseLeave(e)}
       />
       {displayInfo && (
         <div className="movie-info">
-          <h2>{title}</h2>
+          <h2>{artworkForFrame.title}</h2>
+          <ul>
+            <li>{artworkForFrame.artist}</li>
+            <li>{artworkForFrame.museum}</li>
+          </ul>
         </div>
       )}
     </div>
